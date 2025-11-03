99191969285e3c6637ee7bbb99196b4fa9f3f11b
Himadrish Laha <himadrish@gmail.com>
2025-11-03 00:15:09 -0500

    Updated question

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ client = OpenAI(api_key="Your Key")
 response = client.responses.create(
     model="gpt-4o-mini",
     tools=[{"type": "web_search_preview"}],
-    input="What was a President Trump news story from today?"
+    input="What is today's day?"
 )
 
 print(response.output_text)
